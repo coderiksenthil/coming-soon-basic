77e7219edd6cb97e37d168bd9e658ce525b205d5
U.Senthil Kumar <66522620+coderiksenthil@users.noreply.github.com>
2024-12-01 18:22:04 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./App.css";
+
+class App extends React.Component {
+  render = () => {
+    return (
+      <div className="card">
+        <div className="content">
+          <div className="title-holder">
+            <h1>Coming Soon</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
